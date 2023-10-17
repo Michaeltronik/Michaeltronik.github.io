20817f15aff8774f53f03943e6f48ca343fcf5dd
Michaeltronik <148280022+Michaeltronik@users.noreply.github.com>
2023-10-17 15:24:59 -0500

    initial commit

---
+++ strings.js
@@ -0,0 +1,16 @@
+let caja = document.getElementById("caja");
+let boton = document.getElementById("boton");
+const boton1=document.querySelector('#boton1');
+boton.onclick = function () {
+    caja.style.backgroundColor = "red";
+    
+};
+
+
+boton1.addEventListener('click',function(){
+    alert('boton clickeado!');
+    window.alert("hola josue!!!");
+    //boton1.style.backgroundColor = "white";
+});
+
+
